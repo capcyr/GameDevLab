a9ab749216f1a0efacd4f218aabdf17c5ab8a688
capcyr <capcyrusli@gmail.com>
2025-10-11 10:54:51 +0100

    Update quizcode.py

--- Quiz Game/quizcode.py
+++ Quiz Game/quizcode.py
@@ -11,7 +11,21 @@ option2 = Rect(370,300,350,180)
 option3 = Rect(0,500,350,180)
 option4 = Rect(370,500,350,180)
 skip = Rect(750,300,200,750)
+#variables
+score_1 = 0
+timee = 30
+gameeover = False
+listing = []
+indexing = 0
+totalquestioncount = 0
+questionquestion = "question.txt"
+options = [option1,option2,option3,option4]
+
+
+
+
 def draw():
+    global timee
     screen.draw.filled_rect(marqueebox,"Blue")
     screen.draw.filled_rect(questionbox,"Red")
     screen.draw.filled_rect(timerbox,"White")
@@ -20,6 +34,23 @@ def draw():
     screen.draw.filled_rect(option3,"Green")
     screen.draw.filled_rect(option4,"Green")
     screen.draw.filled_rect(skip,"Yellow")
+    #Textboxes
+    message = "welcome to the quiz app"
+    screen.draw.textbox(message,marqueebox,color = "black")
+    screen.draw.textbox(str(timee),timerbox,color = "black")
+#Function for marquee box
+def moving():
+    marqueebox.x-=5
+    if marqueebox.right < 0:
+        marqueebox.left = 900
+
+def countdown():
+    global timee
+    if timee>0:
+        timee-=1
+
 def update():
     pass
+    moving()
+clock.schedule_interval(countdown,1)
 pgzrun.go()
