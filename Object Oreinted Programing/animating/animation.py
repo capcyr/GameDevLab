49d59c2b7a8d62b09ad0f7bb6c12b0d8435219d4
capcyr <capcyrusli@gmail.com>
2026-01-24 10:21:10 +0000

    Update animation.py

--- Object Oreinted Programing/animating/animation.py
+++ Object Oreinted Programing/animating/animation.py
@@ -14,43 +14,63 @@ birthday = pygame.transform.scale(birthday,(800,750))
 christmas = pygame.transform.scale(christmas,(800,750))
 easter = pygame.transform.scale(easter,(800,750))
 halloween = pygame.transform.scale(halloween,(800,750))
-
+clock = pygame.time.Clock()
 run = True
-
+timing = 2000
+switch = pygame.time.get_ticks()
+scene = 0
+font1 = pygame.font.SysFont("comicsans",45)
+font2 = pygame.font.SysFont("Italic",65)
+font3 = pygame.font.SysFont("TimesNewRoman",55)
+font4 = pygame.font.SysFont("Aptos",55)
 while run:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    font1 = pygame.font.SysFont("comicsans",45)
-    font2 = pygame.font.SysFont("Italic",65)
-    font3 = pygame.font.SysFont("TimesNewRoman",55)
-    font4 = pygame.font.SysFont("Aptos",55)
-    screen.blit(birthday,(0,0))
-    text = font1.render("Happy",True,"black")
-    text2 = font1.render("Birthday",True,"black")
-    #blitting
-    screen.blit(text,(320,350))
-    screen.blit(text2,(320,410))
-    pygame.display.update()
+    currenttime = pygame.time.get_ticks()
+    if currenttime-switch>timing:
+        scene+=1
+        switch = currenttime
+    if scene == 0:
+        
+        screen.blit(birthday,(0,0))
+        text = font1.render("Happy",True,"black")
+        text2 = font1.render("Birthday",True,"black")
+        #blitting
+        screen.blit(text,(320,350))
+        screen.blit(text2,(320,410))
+        pygame.display.update()
+    elif scene ==1:
     #image2
-    pygame.time.delay(2000)
-    screen.blit(christmas,(0,0))
-    text3 = font2.render("Merry",True,"black")
-    text4 = font2.render("Christmas",True,"black")
-    screen.blit(text3,(250,380))
-    screen.blit(text4,(250,430))
+        screen.blit(christmas,(0,0))
+        text3 = font2.render("Merry",True,"black")
+        text4 = font2.render("Christmas",True,"black")
+        screen.blit(text3,(250,380))
+        screen.blit(text4,(250,430))
+        pygame.display.update()
+    elif scene ==2:
+        screen.blit(easter,(0,0))
+        text5 = font3.render("Happy",True,"black")
+        text6 = font3.render("Easter",True,"black")
+        screen.blit(text5,(240,130))
+        screen.blit(text6,(240,190))
+        pygame.display.update()
+    elif scene ==3:
+        screen.blit(halloween,(0,0))
+        text5 = font3.render("Happy",True,"black")
+        text6 = font3.render("Halloween",True,"black")
+        screen.blit(text5,(300,320))
+        screen.blit(text6,(300,380))
+        pygame.display.update()
+    
+    
+    
+    else:
+        run = False
     pygame.display.update()
-    pygame.time.delay(2000)
-    screen.blit(easter,(0,0))
-    text5 = font3.render("Happy",True,"black")
-    text6 = font3.render("Easter",True,"black")
-    screen.blit(text5,(250,150))
-    screen.blit(text6,(250,210))
-
-
-
-    pygame.display.update()
-    pygame.time.delay(2000)
-    pygame.time.delay(2000)
 pygame.quit()
+
+
+
 
