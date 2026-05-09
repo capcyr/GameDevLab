5826d0617d621c07e99bb01c1ac0d0aa2b649d51
capcyr <capcyrusli@gmail.com>
2026-05-09 11:56:18 +0100

    Update car.py

--- Object Oreinted Programing/Car Racing Game/car.py
+++ Object Oreinted Programing/Car Racing Game/car.py
@@ -4,6 +4,9 @@ import time
 
 clock = pygame.time.Clock()
 
+enemy_speed = 5
+last_spawn_time = time.time()
+
 pygame.init()
 screen = pygame.display.set_mode((500,650))
 pygame.display.set_caption("Car Game")
@@ -20,12 +23,15 @@ scaledimage3 = pygame.transform.rotate(pygame.transform.scale(yellowcar,(75,150)
 
 
 carrect1 = redcar.get_rect()
-carrect1.x = 0
-carrect1.y = 0
+carrect1.x = 150
+carrect1.y = 500
 
 carrect2 = yellowcar.get_rect()
 carrect2.x = 0
 carrect2.y = 0
+
+#alternate spawn locations
+
 
 bgy1=0
 bgy2 = -650
@@ -40,17 +46,47 @@ def assets():
     
     screen.blit(scaledimage,(0,bgy1))
     screen.blit(scaledimage,(0,bgy2))
-    screen.blit(scaledimage2,(200,500),carrect1)
+    screen.blit(scaledimage2,carrect1)
     screen.blit(scaledimage3,carrect2)
 
-    
+def spawn():
+    global last_spawn_time
+    carrect2.y += enemy_speed
+    if carrect2.y > 650:
+        if time.time()-last_spawn_time >=2:
+            spawning = random.randint(1,3)
+            if spawning == 1:
+                carrect2.x = 150
+                carrect2.y = 0  
+            elif spawning == 2:
+                carrect2.x = 50
+                carrect2.y = 0 
+            elif spawning == 3:
+                carrect2.x = 200
+                carrect2.y = 0 
+            carrect2.y = -150
+            last_spawn_time = time.time()  
+    #screen.blit(scaledimage3,carrect2)
+        
 
+    
 while True:
     clock.tick(120)
+  
     screen.fill("black")
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a] and carrect1.x>90:
+            carrect1.x-=7
+
+        if keys[pygame.K_d] and carrect1.x<300:
+            carrect1.x+=7
+
+
+
     bgy1+=bgspeed
     bgy2+=bgspeed
     if bgy1 >= 650:
@@ -58,9 +94,12 @@ while True:
     if bgy2 >=610:
         bgy2=bgy1-650
 
+
+    spawn()
     assets()
     pygame.display.update()
    
+
 
 
 
