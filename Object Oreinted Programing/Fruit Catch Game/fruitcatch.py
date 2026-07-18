906d4c936d4cb8422ce13951b6268785f5f114b8
capcyr <capcyrusli@gmail.com>
2026-07-18 12:01:03 +0100

    Update fruitcatch.py

--- Object Oreinted Programing/Fruit Catch Game/fruitcatch.py
+++ Object Oreinted Programing/Fruit Catch Game/fruitcatch.py
@@ -15,14 +15,82 @@ lemon = pygame.image.load("images/lemon.png")
 orange = pygame.image.load("images/orange.png")
 watermelon = pygame.image.load("images/watermelon.png")
 scoring = 0
+ranfruit = 0
+scaledimage = pygame.transform.scale(background,(1000,750))
+
+scaledimage2 = pygame.transform.scale(apple,(100,106.944444444))
+scaledimage3 = pygame.transform.scale(lemon,(100,100))
+scaledimage4 = pygame.transform.scale(orange,(100,96.653543307))
+scaledimage5 = pygame.transform.scale(watermelon,(100,80))
+
+basketimage = pygame.transform.scale(basket,(150,90))
+fruitlist = [scaledimage2, scaledimage3,scaledimage4, scaledimage5]
+fruitchoose = random.choice(fruitlist)
+fruit_x = random.randint(50,900)
+fruit_y = -100
+fruitspeed = 5
+
+basketimage_x = 500
+basketimage_y = 650
+
+
+
+
+
+
+clock = pygame.time.Clock()
 
 while run:
-    screen.blit(background,(0,0))
+    clock.tick(120)
+
     
+    screen.blit(scaledimage,(0,0))
+
+    screen.blit(basketimage,(basketimage_x,basketimage_y))
     
+    fruit_y +=fruitspeed
+    screen.blit(fruitchoose,(fruit_x,fruit_y))
+
+
+    if fruit_y > 750:
+        fruitchoose = random.choice(fruitlist)
+        fruit_x = random.randint(50,900)
+        fruit_y = -100
+        
+
+        
+
+    fruit_rect = fruitchoose.get_rect(topleft = (fruit_x,fruit_y))
+    basketrect = basketimage.get_rect(topleft = (basketimage_x,basketimage_y))
+
+
+    if fruit_rect.colliderect(basketrect):
+        fruitchoose = random.choice(fruitlist)
+        fruit_x = random.randint(50,900)
+        fruit_y = -100
+        scoring+=1
+
+
+    scoretext = scoringfont.render(f"Score:{scoring}",True,"white")
+    screen.blit(scoretext, (50,20))
+
+
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_a] and basketimage_x>50:
+        basketimage_x-=15
+
+    if keys[pygame.K_d] and basketimage_x<825:
+        basketimage_x+=15
+
+
+        
     
+
     pygame.display.update()
 pygame.quit()
